7d75eb53f9caad9ce10144e0b7d7aab4c6ad3125
Thaiyal-Naayagi <163297785+Thaiyal-Naayagi@users.noreply.github.com>
2026-09-03 10:52:13 +0530

    Update router_sync.v

--- design/router_sync.v
+++ design/router_sync.v
@@ -1,22 +1,22 @@
-`timescale 1ns / 1ps
+
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
+// Company:
+// Engineer:
+//
 // Create Date: 18.08.2025 11:38:43
-// Design Name: 
+// Design Name:
 // Module Name: synchronizer
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
+// Project Name:
+// Target Devices:
+// Tool Versions:
+// Description:
+//
+// Dependencies:
+//
 // Revision:
 // Revision 0.01 - File Created
 // Additional Comments:
-// 
+//
 //////////////////////////////////////////////////////////////////////////////////
 
 
@@ -27,15 +27,15 @@
     output reg [2:0] write_en;
     output vld_out_0, vld_out_1, vld_out_2;
     output reg  fifo_full, soft_reset_0, soft_reset_1, soft_reset_2;
-    
+
     reg [4:0] count_0, count_1, count_2;
     reg [1:0] temp;
-    
+
     //valid out based on whether the fifo is empty or not, if it is not empty then it become valid and the valid out is used to generate read_en in fsm
     assign vld_out_0 = ~empty_0;
     assign vld_out_1 = ~empty_1;
     assign vld_out_2 = ~empty_2;
-   
+
    // based on data in value, it decides which fifo's full value to be assigned
     always @(*) begin
         fifo_full = 0;
@@ -52,8 +52,8 @@
         if(!rst) temp <= 2'b11;
         else if(detect_address) temp <= data_in;
     end
-    
-    //based on write_en_reg and data in any one of three fifo will be enabled 
+
+    //based on write_en_reg and data in any one of three fifo will be enabled
     always @(*) begin
         write_en = 3'b000;
         if(write_en_reg) begin
@@ -65,31 +65,30 @@
             endcase
         end
     end
-    
+
     //soft reset conditions
-    
+
     always @(posedge clk)begin
-        if(!rst) begin 
+        if(!rst) begin
             count_0 <= 1'b0;
             soft_reset_0 <= 1'b0;
         end
          else if(vld_out_0) begin
             soft_reset_0 <= 1'b0;
             if(!read_enb_0) begin
-             count_0 <= count_0 + 1'b1;
-             
-                if(count_0 == 29) begin
+                if(count_0 < 29) count_0 <= count_0 + 1'b1;
+                if(count_0 >= 29) begin
                    count_0 <= 1'b0;
                    soft_reset_0 <= 1'b1;
                 end
-            
+
             end
-            else count_0 <= 1'b0; 
+            else count_0 <= 1'b0;
          end
          else begin
             count_0 <= 1'b0;
             soft_reset_0 <= 1'b0;
-         end 
+         end
     end
 
     always @(posedge clk)begin
@@ -100,20 +99,19 @@
          else if(vld_out_1) begin
             soft_reset_1 <= 1'b0;
             if(!read_enb_1) begin
-             count_1 <= count_1 + 1'b1;
-             
-                if(count_1 == 29) begin
+             if(count_1 < 29)count_1 <= count_1 + 1'b1;
+                if(count_1 >= 29) begin
                    count_1 <= 1'b0;
                    soft_reset_1 <= 1'b1;
                 end
-            
+
             end
-            else count_1 <= 1'b0; 
+            else count_1 <= 1'b0;
          end
          else begin
             count_1 <= 1'b0;
             soft_reset_1 <= 1'b0;
-         end 
+         end
     end
 
     always @(posedge clk)begin
@@ -124,20 +122,20 @@
          else if(vld_out_2) begin
             soft_reset_2 <= 1'b0;
             if(!read_enb_2) begin
-             count_2 <= count_2 + 1'b1;
-             
-                if(count_2 == 29) begin
+                if(count_2 < 29)count_2 <= count_2 + 1'b1;
+                if(count_2 >= 29) begin
                    count_2 <= 1'b0;
                    soft_reset_2 <= 1'b1;
                 end
-            
+
             end
-            else count_2 <= 1'b0; 
+            else count_2 <= 1'b0;
          end
          else begin
             count_2 <= 1'b0;
             soft_reset_2 <= 1'b0;
-         end 
+         end
     end
-    
-endmodule 
+
+endmodule
+
